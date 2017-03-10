eaf884e6bd6eb0453b22d3ce596cee81f1d1641f
André Kilchenmann <a.kilchenmann@unibas.ch>
2017-03-10 08:50:42 +0100

    fix (authentication check on project interface) disable the project header, if the user is not logged in

--- src/app/view/dashboard/project/project-profile/project-profile.component.ts
+++ src/app/view/dashboard/project/project-profile/project-profile.component.ts
@@ -1,5 +1,23 @@
+/* Copyright © 2016 Lukas Rosenthaler, André Kilchenmann, Andreas Aeschlimann,
+ * Sofia Georgakopoulou, Ivan Subotic, Benjamin Geer, Tobias Schweizer.
+ * This file is part of SALSAH.
+ * SALSAH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * SALSAH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * You should have received a copy of the GNU Affero General Public
+ * License along with SALSAH.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
 import {Component, OnInit} from '@angular/core';
-import {ProjectItem} from "../../../../model/classes/projects";
+import {Params, ActivatedRoute, Router} from "@angular/router";
+import {ProjectItem, Project} from "../../../../model/classes/projects";
+import {ProjectsService} from "../../../../model/api/projects.service";
+import {ApiServiceResult} from "../../../../model/api/api-service-result";
+import {ApiServiceError} from "../../../../model/api/api-service-error";
 
 @Component({
     selector: 'salsah-project-profile',
@@ -8,15 +26,45 @@ import {ProjectItem} from "../../../../model/classes/projects";
 })
 export class ProjectProfileComponent implements OnInit {
 
-    project: ProjectItem;
 
-    constructor() {
-        if( JSON.parse(localStorage.getItem('project')) !== null ) {
-            this.project = JSON.parse(localStorage.getItem('project'));
-        }
+    errorMessage: string = undefined;
+    projectItem: ProjectItem = new ProjectItem();
+
+    project: Project = new Project();
+
+    projectRoute: string = '/project/';
+
+    constructor(
+        private _router: Router,
+        private _route: ActivatedRoute,
+        private _projectsService: ProjectsService
+    ) {
+
     }
 
     ngOnInit() {
+
+        this.projectItem = JSON.parse(localStorage.getItem('project'));
+
+        console.log(this.projectItem);
+
+        if(this.projectItem !== null) {
+            // get the project information
+            this._projectsService.getProject(this.projectItem.shortname)
+                .subscribe(
+                    (result: ApiServiceResult) => {
+                        this.project = result.getBody(Project);
+                        console.log(this.project);
+                    },
+                    (error: ApiServiceError) => {
+                        this.errorMessage = <any>error;
+                        localStorage.removeItem('project');
+                    }
+                );
+        }
+        else {
+            console.log('Error in local storage with the project item');
+        }
 
 
     }
