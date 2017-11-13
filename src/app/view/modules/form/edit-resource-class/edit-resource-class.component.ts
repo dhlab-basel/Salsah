162c40632bc43bafd0d9d3f399730f912d5d946f
Sofia Georgakopoulou <s.georgakopoulou@unibas.ch>
2017-11-13 11:32:26 +0100

    Wip/fix issue#90 (#93)
    * Add expansion panels to property edit (wip-1)
    
    * UPDATE edit form - fix css - add buttons - wip
    
    * Re-style edit resource and properties with drop down cards. Add property card added
    
    * ADD dnd to edit part, in the accordeon panels
    
    * UPDATE list of guiItems
    
    * ADD reset button that resets or clears form
    
    * ADD required fields and disabled save button in add new  property form
    
    * Implement FormBuilder in the resource info edit panel
    
    * Add initial list styling with grid list

--- src/app/view/modules/form/edit-resource-class/edit-resource-class.component.ts
+++ src/app/view/modules/form/edit-resource-class/edit-resource-class.component.ts
@@ -12,12 +12,12 @@
  * License along with SALSAH.  If not, see <http://www.gnu.org/licenses/>.
  * */
 
-import {Component, EventEmitter, OnChanges, Input, Output} from '@angular/core';
+import {Component, EventEmitter, OnChanges, Inject, Input, Output, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ApiServiceResult} from "../../../../model/services/api-service-result";
 import {ApiServiceError} from "../../../../model/services/api-service-error";
 import {ResourceTypesService} from "../../../../model/services/resource-types.service";
 import {Properties, ResourceType, ResourceTypeInfo} from "../../../../model/webapi/knora/";
-// import {ResourceType, ResourceTypeInfo} from "../../../../model/webapi/knora/";
 import {ProjectsService} from "../../../../model/services/projects.service";
 import {ProjectsList, ProjectItem} from "../../../../model/webapi/knora/";
 
@@ -34,22 +34,6 @@ import {
     selector: 'salsah-edit-resource-class',
     templateUrl: './edit-resource-class.component.html',
     styleUrls: ['./edit-resource-class.component.scss'],
-    animations: [
-        trigger('resEdit',
-            [
-                state('inactive', style({display: 'none'})),
-                state('active', style({display: 'block'})),
-                transition('inactive => true', animate('100ms ease-in')),
-                transition('true => inactive', animate('100ms ease-out'))
-            ]),
-        trigger('propEdit',
-            [
-                state('inactive', style({display: 'none'})),
-                state('active', style({display: 'block'})),
-                transition('inactive => true', animate('100ms ease-in')),
-                transition('true => inactive', animate('100ms ease-out'))
-            ])
-    ]
 })
 
 
@@ -60,40 +44,91 @@ export class EditResourceClassComponent implements OnChanges {
     @Input('projects') projectsList: ProjectItem[];
 
 
+
     resType: ResourceTypeInfo = new ResourceTypeInfo;
 
     properties: Properties;
     errorMessage: string = undefined;
-    selectedRow: number;
+    resIcon: string = undefined;
 
     editResource: boolean;
 
-    focusOnResourceEdit: string = 'inactive';
-    focusOnPropertiesEdit: string = 'inactive';
+    eRForm4class: FormGroup;
 
-    editResourceClass: ResourceTypeInfo = new ResourceTypeInfo();
+
+    public editResFormLabels: any = {
+        label: 'Resource',
+        description:'Click to edit the resource name',
+        resource: {
+            label: 'Resource label',
+            description: 'Resource description',
+            icon: 'Icon',
+            properties: {
+                formLabel: 'Properties',
+                formDescription: 'Click to edit the property',
+                label: 'Property label',
+                description: 'Property description',
+                gui: 'GUI element',
+                attributes: 'GUI attributes',
+                id: 'Property ID',
+                occurrence: 'Occurrence',
+                valuetype_id: 'Value type ID',
+                vocabulary: 'Vocabulary',
+                icon: 'Icon'
+            }
+        },
+        addProp:{
+            label: 'Add new property',
+            description: 'Add new or create a custom property'
+        },
+        buttons:{
+            save: "Save",
+            reset: "Reset",
+            close: "Close",
+            edit: "Edit",
+        }
+};
 
     guiItems: string[] = [
         "text",
         "richtext",
-        "text area",
-        "dropdown menu",
-        "number field",
-        "date field",
+        "textarea",
+        "number",
         "searchbox",
-        "file upload"
-    ];
+        "fileupload",
+        "date",
+        "radio",
+        "hlist",
+        "pulldown",
+        "spinbox",
+        "richtext",
+        "checkbox",
+        "interval",
+        "colorpicker",
+        "geometry"
+        ];
 
     cardinalityList: string[] = [
         "1",
         "1-n",
         "0-1",
         "0-n"
-    ];
+        ];
 
+    // the following form fields would have an error check
+    formErrors = {
+        'resLabel': ''
+    };
+    // ...with the following messages
+    validationMessages = {
+        'resLabel': {
+            'required': 'Resource label is required'
+        }
+    };
 
     constructor(private _resourceTypesService: ResourceTypesService,
-                private _projectsService: ProjectsService) {
+                private _projectsService: ProjectsService,
+                private _fb: FormBuilder) {
     }
 
 
@@ -104,13 +139,18 @@ export class EditResourceClassComponent implements OnChanges {
             .subscribe(
                 (result: ApiServiceResult) => {
                     this.resType = result.getBody(ResourceType).restype_info;
-//                    let tmpResTypeInfo: ResourceTypeInfo = result.getBody(ResourceTypeInfo);
-                    this.selectedRow = this.index;
-                    this.editResourceClass.properties= this.resType.properties;
+                    this.resIcon = this.resType.icon;
 
-                    // disable the progress loader of the submodule
-//                    this.loadingSubmodule.emit(this.isLoadingSubModule);
+                    //    resource form validation configuration
+                    this.eRForm4class = this._fb.group({
+                        'resLabel': [this.resType.label, Validators.required],
+                        'resIcon' : this.resType.icon,
+                        'resDescription': this.resType.description
+                    });
+                    this.eRForm4class.valueChanges
+                        .subscribe(data => this.onResValueChanged(data));
                 },
+
                 (error: ApiServiceError) => {
                     this.errorMessage = <any>error;
                     // disable the progress loader of the submodule
@@ -123,9 +163,28 @@ export class EditResourceClassComponent implements OnChanges {
                 (result: ApiServiceResult) => {
                     this.projectsList = result.getBody(ProjectsList).projects;
                 }
-            )
-
+            );
     }
+
+    onResValueChanged(data?: any) {
+
+        if (!this.eRForm4class ) {
+            return;
+        }
+
+        const form = this.eRForm4class;
+        Object.keys(this.formErrors).map(field => {
+            this.formErrors[field] = '';
+            const control = form.get(field);
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                Object.keys(control.errors).map(key => {
+                    this.formErrors[field] += messages[key] + ' ';
+                });
+            }
+        });
+    }
+
 
     editResources(){
         this.editResource = true;
@@ -135,41 +194,78 @@ export class EditResourceClassComponent implements OnChanges {
     }
 
     //form functions
-    onSubmit(data: any): void {
-        console.log('your new resource is:', this.resType);
-//        this.dialog.closeAll();
-    }
-
-    resEdit: any;
-    propEdit: any;
-
-    toggleMenu(name: string) {
-        switch (name) {
-            case 'resEdit':
-                this.focusOnPropertiesEdit = 'inactive';
-                this.focusOnResourceEdit = (this.focusOnResourceEdit === 'active' ? 'inactive' : 'active');
-                break;
-            case 'propEdit':
-                this.focusOnResourceEdit = 'inactive';
-                this.focusOnPropertiesEdit = (this.focusOnPropertiesEdit === 'active' ? 'inactive' : 'active');
-                break;
+    submitResEdit(): void {
+        if (this.eRForm4class.valid && this.resType !== undefined) {
+            console.log('Your submitted data is: ', this.eRForm4class.value);
+        }
+        else {
+            console.log('Form not valid');
         }
     }
+
+    submitPropEdit(index: number, event): void {
+        if (this.resType.properties[index] !== undefined) {
+            console.log('Your submitted property is:', this.resType.properties[index]);
+        }
+    }
+
+    onSubmit(data: any): void {
+        console.log('you submitted value:', data);
+    }
+
+    resetDefaultRes(): void{
+        // this.eRForm4class.reset();
+        this._resourceTypesService.getResourceType(this.iri)
+            .subscribe(
+                (result: ApiServiceResult) => {
+                    this.resType = result.getBody(ResourceType).restype_info;
+                    this.resIcon = this.resType.icon;
+
+                    //    resource form validation configuration
+                    this.eRForm4class = this._fb.group({
+                        'resLabel': [this.resType.label, Validators.required],
+                        'resIcon' : this.resType.icon,
+                        'resDescription': this.resType.description
+                    });
+                },
+                (error: ApiServiceError) => {
+                    this.errorMessage = <any>error;
+                }
+            );
+    }
+
+    // At this moment there is a mix-up when reordering the properties with drag and drop and then resetting: the property resets to the one with the respective index
+    // (i.e. if I move a prop[6] from position 6 to position 3, it will reset to prop[3]). This should be fixed when we actually submit the data to Knora, since then
+    // we will get the updated list every time after saving.
+    resetDefaultProp(index: number): void{
+        if (this.resType.properties[index] !== undefined) {
+            this._resourceTypesService.getResourceType(this.iri)
+                .subscribe(
+                    (result: ApiServiceResult) => {
+                        this.resType.properties[index] = result.getBody(ResourceType).restype_info.properties[index];
+                    },
+                    (error: ApiServiceError) => {
+                        this.errorMessage = <any>error;
+                    }
+                );
+        }
+    }
+
 
     //sets default values from json files on mat-select
     setGUI(index: number, event) {
-        if (this.editResourceClass.properties[index] !== undefined) {
-            this.editResourceClass.properties[index].gui_name = event.value;
+        if (this.resType.properties[index] !== undefined) {
+            this.resType.properties[index].gui_name = event.value;
         }
     }
     setOcc(index: number, event) {
-        if (this.editResourceClass.properties[index] !== undefined) {
-            this.editResourceClass.properties[index].occurrence = event.value;
+        if (this.resType.properties[index] !== undefined) {
+            this.resType.properties[index].occurrence = event.value;
         }
     }
     setVoc(index: number, event) {
-        if (this.editResourceClass.properties[index] !== undefined) {
-            this.editResourceClass.properties[index].vocabulary = event.value;
+        if (this.resType.properties[index] !== undefined) {
+            this.resType.properties[index].vocabulary = event.value;
         }
     }
 
