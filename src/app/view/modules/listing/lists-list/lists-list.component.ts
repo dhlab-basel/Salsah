b93fcfe22633078f18192bcba8bd8f9aa4ecd31b
Sofia Georgakopoulou <s.georgakopoulou@unibas.ch>
2018-05-02 12:42:25 +0200

    Set list edit according to user permissions (only admins can edit) (#218)
    * create loggedInUser.projectAdmin to control user permissions, WIP
    
    * edit only on logged in administrator - only dnd does not work yet / WIP
    
    * FIX drag and drop only on admin user
    
    * Show edit button on mouse hover
    
    * Fix conflicts and add edit button on list name (shown on mouse hover)
    
    * FIX minor error in showing list name edit button
    
    * wip | lines 149-158 not working
    
    * fix (lists list): get user data
    
    * list editing by user permission working, dnd still not working
    
    * dnd problem fixed, this is ready
    
    * style (lists list): fix styling issue with onHover-effect

--- src/app/view/modules/listing/lists-list/lists-list.component.ts
+++ src/app/view/modules/listing/lists-list/lists-list.component.ts
@@ -16,10 +16,13 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ListsService} from '../../../../model/services/lists.service';
 import {ApiServiceError} from '../../../../model/services/api-service-error';
 import {MessageData} from '../../message/message.component';
-import {List, ListInfo, ListNode} from '../../../../model/webapi/knora';
+import {List, ListInfo, ListNode, User} from '../../../../model/webapi/knora';
 import {FormDialogComponent} from '../../dialog/form-dialog/form-dialog.component';
 import {MatDialog, MatDialogConfig} from '@angular/material';
 import {TreeNode} from 'angular-tree-component/dist/models/tree-node.model';
+import {AuthenticationService} from '../../../../model/services/authentication.service';
+import {AppConfig} from '../../../../app.config';
+import {UsersService} from '../../../../model/services/users.service';
 
 @Component({
     selector: 'salsah-lists-list',
@@ -29,6 +32,7 @@ import {TreeNode} from 'angular-tree-component/dist/models/tree-node.model';
 export class ListsListComponent implements OnInit {
 
     @Input() restrictedBy?: string;
+    // @Input() projectIri: string;
     @Output() toggleItem = new EventEmitter<any>();
     // @Output() deleteNodeEvent = new EventEmitter<any>();
 
@@ -37,6 +41,36 @@ export class ListsListComponent implements OnInit {
     errorMessage: any;
 
     isExpanded: boolean = false;
+    epExpanded: boolean = false;
+
+    checkUserStatus: boolean;
+    // is there a logged-in user?
+    // look at localStorage and the object currentUser with email, token and sysAdmin (?) info
+    loggedInUser: any;
+    currentUser: User = undefined;
+    projectAdmin: any;
+    userAdmin: boolean;
+
+
+    // default permission groups / role of the user in a project
+    // defaultGroups: AutocompleteItem[] = [
+    //     {
+    //         iri: AppConfig.ProjectMemberGroup,
+    //         name: 'Member'
+    //     },
+    //     {
+    //         iri: AppConfig.ProjectAdminGroup,
+    //         name: 'Administrator'
+    //     }
+    /* use the following in system view only!
+    {
+        iri: AppConfig.SystemAdminGroup,
+        name: '',
+        label: 'System admin'
+    }
+    */
+
+    // ];
 
 
     // in the case of no data, but with a working API
@@ -59,12 +93,9 @@ export class ListsListComponent implements OnInit {
     // iri of the selected list
     iri: string;
 
-    options = {
-        useVirtualScroll: false,
-        nodeHeight: 22,
-        allowDrag: true,
-        allowDrop: true,
-    };
+    nodeDrag: boolean = false;
+    nodeDrop: boolean = false;
+    options: any = {};
 
     public listLists: any = {
         nodesLabel: 'Nodes',
@@ -78,6 +109,8 @@ export class ListsListComponent implements OnInit {
     };
 
     constructor(private _listsService: ListsService,
+                private _authenticationService: AuthenticationService,
+                private _usersService: UsersService,
                 public _dialog: MatDialog) {
     }
 
@@ -112,8 +145,59 @@ export class ListsListComponent implements OnInit {
                 );
         }
 
+        console.log('projectIri:', this.restrictedBy);
+
         this.isExpanded = false;
 
+        // get the user data only if a user is logged in
+        this.loggedInUser = JSON.parse(localStorage.getItem('currentUser'));
+
+        if (this.loggedInUser != null) {
+            if (this.loggedInUser.sysAdmin) { // if the logged in user is a system admin, allow drag and drop here
+                this.nodeDrag = true;
+                this.nodeDrop = true;
+                this.options = {
+                    useVirtualScroll: false,
+                    nodeHeight: 22,
+                    allowDrag: this.nodeDrag,
+                    allowDrop: this.nodeDrop,
+                };
+            }
+            else {
+                this._usersService.getUserByEmail(this.loggedInUser.email).subscribe(
+                    (result: User) => {
+                        // return full user details and check if they have administrative permissions for this project
+                        this.currentUser = result;
+                        this.projectAdmin = this.currentUser.permissions.administrativePermissionsPerProject[this.restrictedBy];
+                        console.log('full user details: ', this.currentUser);
+                        console.log('project Admin details: ', this.projectAdmin);
+
+                        if (this.projectAdmin.length > 0) {
+                            // console.log('projectAdmin ');
+                            this.nodeDrag = true;
+                            this.nodeDrop = true;
+                        } else {
+                            // console.log('NO project or sys Admin');
+                            this.nodeDrag = false;
+                            this.nodeDrop = false;
+                        }
+                        this.options = {
+                            useVirtualScroll: false,
+                            nodeHeight: 22,
+                            allowDrag: this.nodeDrag,
+                            allowDrop: this.nodeDrop,
+                        };
+                    },
+                    (error: ApiServiceError) => {
+                        this.errorMessage = error;
+                    }
+                );
+            }
+        }
+        else{ //if there is no logged in user don't allow drag and drop
+            this.nodeDrag = false;
+            this.nodeDrop = false;
+        }
     }
 
     fetchListData(iri: string) {
@@ -157,14 +241,15 @@ export class ListsListComponent implements OnInit {
         tree.treeModel.update();
     }
 
-    //The code below is deprecated because we moved the delete node button to the edit node form
-    // deleteNode(node: TreeNode, tree): void {
-        // this.deleteNodeEvent.emit({node, tree});
 
-        // if (node.parent != null) {
-        //     node.parent.data.children.splice(node.parent.data.children.indexOf(node.data), 1)
-        //     tree.treeModel.update()
-        // }
+    // The code below is deprecated because we moved the delete node button to the edit node form
+    // deleteNode(node: TreeNode, tree): void {
+    // this.deleteNodeEvent.emit({node, tree});
+
+    // if (node.parent != null) {
+    //     node.parent.data.children.splice(node.parent.data.children.indexOf(node.data), 1)
+    //     tree.treeModel.update()
+    // }
     // }
 
     // TODO: implement proper edit method/component
