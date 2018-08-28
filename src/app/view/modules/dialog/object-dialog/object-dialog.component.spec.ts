2de55f5d344bbe790fc17e4cf294c34a925bb68f
Sepideh Alassi <sepideh.alassi@unibas.ch>
2018-08-28 12:53:18 +0200

    List of abbreviations (#284)
    * feature (beol_LEOOIV_Abbreviations) present the list of abbreviations of LEOO IV in BEOL landing page
    
    * fix (timeout) set timeout for objectDialogComponent for failing tests

--- src/app/view/modules/dialog/object-dialog/object-dialog.component.spec.ts
+++ src/app/view/modules/dialog/object-dialog/object-dialog.component.spec.ts
@@ -8,7 +8,15 @@ describe('ObjectDialogComponent', () => {
     let component: ObjectDialogComponent;
     let dialog: MatDialog;
     let fixture: ComponentFixture<ObjectDialogComponent>;
+    let originalTimeout;
+    beforeEach(function() {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    });
 
+    afterEach(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
