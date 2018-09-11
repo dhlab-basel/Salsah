d9680d6f15e9b6d045a9c9ded0790f050bc16597
André Kilchenmann <github@milchkannen.ch>
2018-09-11 16:17:14 +0200

    fix (packages): add missing knora-ui modules: search and viewer (#290)
    * fix (packages): add missing knora-ui modules: search and viewer
    
    * docs (gemfile): fix vulnerability issue
    
    * fix (travis): removed not needed fixes
    
    * test (travis): fix timeout issue

--- src/app/view/dashboard/user/user-collections/user-collections.component.spec.ts
+++ src/app/view/dashboard/user/user-collections/user-collections.component.spec.ts
@@ -17,6 +17,17 @@ describe('UserCollectionsComponent', () => {
         'sysAdmin': false
     };
 
+    let originalTimeout;
+
+    beforeEach(function () {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    });
+
+    afterEach(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
